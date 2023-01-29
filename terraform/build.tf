83bc84acbcc23a41ba5d088818fc07d7da32e98e
Lukas Karlsson <lukwam@gmail.com>
2023-01-29 23:29:52 +0000

    Added hello_world_v2 cloud function.

--- terraform/build.tf
+++ terraform/build.tf
@@ -80,3 +80,34 @@ resource "google_cloudbuild_trigger" "deploy_hello_world_function" {
   }
 
 }
+
+resource "google_cloudbuild_trigger" "deploy_hello_world_v2_function" {
+  provider           = google
+  name               = "deploy-hello-world-v2-function"
+  description        = "Deploy hello_world_v2 Function"
+  filename           = "functions/hello_world_v2/cloudbuild.yaml"
+  project            = google_project_service.service["cloudbuild.googleapis.com"].project
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+
+  included_files = [
+    "functions/hello_world_v2/**",
+  ]
+  ignored_files = [
+    "functions/hello_world_v2/*.md",
+    "functions/hello_world_v2/*.sh",
+    "functions/hello_world_v2/Dockerfile",
+  ]
+
+  github {
+    name  = "hera"
+    owner = "lukwam"
+    push {
+      branch = "^main$"
+    }
+  }
+
+  substitutions = {
+    _REGION = var.region
+  }
+
+}
